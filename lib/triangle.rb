c0dd0c6b7dbda33bb404558b976add8cb1cee1aa
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-21 08:16:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,23 +1,32 @@
 class Triangle
   # attr_accessor :side1, :side2, :side3
-  def initilize(side1, side2, side3)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
-  end
-  def kind
-   if @side1 == 0 || @side2 == 0 || @side3 == 0 || @side1+@side2 <= @side3 || @side1+@side3 <= @side2 || @side2 + @side3 <= @side1
-     raise TriangleError
-     outs
-   elsif @side1 == @side2 && @side2 == @side3
-     :equilateral
-   elsif
-     @side1 == @side2  || @side2 == @side3 || @side1 == @side3
-     :isosceles
-   else
-     :scalene
-   end
- end
+  def initialize(side1, side2, side3)
+      @sides = []
+      @side1 = side1
+      @side2 = side2
+      @side3 = side3
+      @sides << side1 
+      @sides << side2
+      @sides << side3
+    end
+  
+    def kind 
+      if @sides.any? {|e| e == 0 } || 
+          @sides.any? {|e| e < 0 } ||
+          @side1 + @side2 <= @side3 || @side2 + @side3 <= @side1 || @side1 + @side3 <= @side2
+        begin
+          raise TriangleError
+          puts error.message
+        end
+      elsif @side1 == @side2 && @side2 == @side3
+        :equilateral
+      elsif 
+        @side1 != @side2 && @side2 != @side3 && @side1 != @side3
+        :scalene
+      else
+        :isosceles
+      end
+    end
  class TriangleError < StandardError
    def error_bud
      Return " Your triangles seems to be a little off.... Sorry bud! Might wanna check your input. :)"
