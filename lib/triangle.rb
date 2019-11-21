8bc0a8a9eccf1ad6c59181c3fa11e2383e97fcce
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-21 08:17:40 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
-end
+  # attr_accessor :side1, :side2, :side3
+  def initialize(side1, side2, side3)
+      @sides = []
+      @side1 = side1
+      @side2 = side2
+      @side3 = side3
+      @sides = [side1, side2, side3]
+    end
+
+    def kind
+      if @sides.any? {|e| e == 0 } ||
+          @sides.any? {|e| e < 0 } ||
+          @side1 + @side2 <= @side3 || @side2 + @side3 <= @side1 || @side1 + @side3 <= @side2
+        begin
+          raise TriangleError
+          puts error_bud
+        end
+      elsif @side1 == @side2 && @side2 == @side3
+        :equilateral
+      elsif
+        @side1 != @side2 && @side2 != @side3 && @side1 != @side3
+        :scalene
+      else
+        :isosceles
+      end
+    end
+ class TriangleError < StandardError
+   def error_bud
+     Return " Your triangles seems to be a little off.... Sorry bud! Might wanna check your input. :)"
+   end
+ end
+ end
