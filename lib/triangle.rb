71bf35efeaaf216e2976d04b9369ec59d1e41af9
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-21 08:14:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,19 +4,19 @@ class Triangle
     @side1 = side1
     @side2 = side2
     @side3 = side3
-    @all_sides = [@side1,@side2,@side3]
   end
   def kind
-    if @all_sides.any? {|number|number <= 0 || @side1 + @side2 <= @side3 || @side2 + @side3 <= @side1 || @side1 + @side3 <= @side2}
-      raise TriangleError
-    elsif @side1 == @side2 && @side2 == @side3
-       return :equilateral
-     elsif @side1 == @side3 && @side2 != @side1 || @side1 == @side2 && @side3 != @side1 || @side2 == @side3 && @side1 != @side2
-       return :isosceles
-     else
-       return :scalene
-     end
-   end
+   if @side1 == 0 || @side2 == 0 || @side3 == 0 || @side1+@side2 <= @side3 || @side1+@side3 <= @side2 || @side2 + @side3 <= @side1
+     raise TriangleError
+   elsif @side1 == @side2 && @side2 == @side3 
+     :equilateral 
+   elsif
+     @side1 == @side2  || @side2 == @side3 || @side1 == @side3
+     :isosceles
+   else 
+     :scalene
+   end  
+ end  
  class TriangleError < StandardError
    def error_bud
      Return " Your triangles seems to be a little off.... Sorry bud! Might wanna check your input. :)"
