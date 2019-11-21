0727de9a725d72d690bce4af4ef209d96f1f2e37
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-21 08:17:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,9 +5,7 @@ class Triangle
       @side1 = side1
       @side2 = side2
       @side3 = side3
-      @sides << side1
-      @sides << side2
-      @sides << side3
+      @sides = [side1, side2, side3]
     end
 
     def kind
